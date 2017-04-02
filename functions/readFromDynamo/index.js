f44e516c01e5f679721929cc0cdc74fe5adc4647
derek clark <derekclark14@googlemail.com>
2017-04-02 13:23:16 +0100

    new lambda to read all records from dynamodb

---
+++ functions/readFromDynamo/index.js
@@ -0,0 +1,21 @@
+'use strict'
+ console.log("starting function");
+
+  const AWS = require('aws-sdk');
+  // var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+  const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
+
+exports.handler = function(event, context, callback) {
+    let scanningParams = {
+      TableName: 'MyPlaces',
+      Limit: 100
+    }
+
+    docClient.scan(scanningParams, function(err, data){
+      if(err){
+        callback(err, null);
+      }else{
+        callback(null, data);
+      }
+    })
+};
